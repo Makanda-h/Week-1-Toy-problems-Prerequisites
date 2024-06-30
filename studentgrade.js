543882cbef22b9be33020315609b7ac2fac06647
Hope <hopemwaura39@gmail.com>
2024-06-30 18:21:39 +0300

    student grade

---
+++ studentgrade.js
@@ -0,0 +1,26 @@
+const marks = prompt("Enter marks (0-100):");
+  if (marks < 0 || marks > 100) {
+    console.log("Enter number between 0 and 100");
+  }
+
+function studentGrade() {
+  let grade = "F";
+  if (marks > 79) {
+    grade = "A";
+  } else {
+    if (marks >= 60 && marks <= 79) {
+      grade = "B";
+    } else {
+      if (marks >= 49 && marks <= 59) {
+        grade = "C";
+      } else {
+        if (marks >= 40 && marks <= 48) {
+          grade = "D";
+        } else {
+          grade = "E";
+        }
+      }
+    }
+  }
+}
+studentGrade()
